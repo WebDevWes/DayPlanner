519339a731179d2fdaf5c33eda2e4aabf15291ff
Wesley <wesaboo@gmail.com>
2020-05-09 04:10:49 -0700

    cant grab from local storage yet

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ navDate.text(currentDate);
 //Dynamically create planner div
 
 for (let time = 9; time <= 17; time++) {
-    
+
     //Div for the row
     var plannerDiv = $("<div>");
     plannerDiv.addClass("row container");
@@ -17,36 +17,61 @@ for (let time = 9; time <= 17; time++) {
     //am or pm display
     var hour12 = 0;
     var ampm = "";
-    if (time > 12){
+    if (time > 12) {
         hour12 = time - 12;
         am_pm = " pm";
     } else {
         hour12 = time;
         am_pm = " am";
     }
-    
+
     //Div for the time w/ business hours starting at 9am and ending at 5pm
     var timeDiv = $("<div>");
     timeDiv.addClass("col-lg-2 time");
     timeDiv.attr("hour-data", time)
-    timeDiv.text(hour12+am_pm);
+    timeDiv.text(hour12 + am_pm);
     plannerDiv.append(timeDiv);
 
     //Creating input for each row
     var input = $("<input>");
     input.addClass("col-lg-8 content");
+    input.addClass("hour"+time)
+    input.attr("type", "text")
+    input.attr("hour-data", time)
     plannerDiv.append(input);
 
     //Creating save button for each row
     var save = $("<div>");
     save.addClass("col-lg-1 save");
+    save.attr("hour-data", time)
+    //save.attr("input-data", input[time].textContent);
     plannerDiv.append(save);
     save.text("Save");
-    
-    //Changing color of each row to determine past/present/future  *** how is 9 greater than 17????
-    if (timeDiv.attr("hour-data") === moment().format("H")){
-        input.css("background-color","green");
-    } else if (timeDiv.attr("hour-data") < moment().format("H")) {
+
+    //Changing color of each row to determine past/present/future 
+    if (timeDiv.attr("hour-data") === moment().format("H")) {
+        input.css("background-color", "green");
+    } else if (timeDiv.attr("hour-data") > moment().format("H")) {
         input.css("background-color", "orange");
     }
-}
+
+    //Local storage
+    save.on("click", function () {
+        var dataHour = $(this).attr("hour-data");
+        var content = $(".hour"+time).val()
+
+        localStorage.setItem(dataHour, content);
+        console.log(dataHour, content);
+    })
+};
+
+//Populate input from local storage
+$(".hour9").append(JSON.parse(localStorage.getItem("9")));
+$(".hour10").append(JSON.parse(localStorage.getItem("10")));
+$(".hour11").append(localStorage.getItem("11"));
+$(".hour12").append(localStorage.getItem("12"));
+$(".hour13").append(localStorage.getItem("13"));
+$(".hour14").append(localStorage.getItem("14"));
+$(".hour15").append(localStorage.getItem("15"));
+$(".hour16").append(localStorage.getItem("16"));
+$(".hour17").append(localStorage.getItem("17"));
