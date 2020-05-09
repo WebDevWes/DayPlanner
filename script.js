067409f8fe529750d7e007008da3610abd1528ac
Wesley <wesaboo@gmail.com>
2020-05-09 10:15:12 -0700

    got getItem working

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ for (let time = 9; time <= 17; time++) {
     //Div for the time w/ business hours starting at 9am and ending at 5pm
     var timeDiv = $("<div>");
     timeDiv.addClass("col-lg-2 time");
-    timeDiv.attr("hour-data", time)
+    timeDiv.attr("data-hour", time)
     timeDiv.text(hour12 + am_pm);
     plannerDiv.append(timeDiv);
 
@@ -37,41 +37,43 @@ for (let time = 9; time <= 17; time++) {
     input.addClass("col-lg-8 content");
     input.addClass("hour"+time)
     input.attr("type", "text")
-    input.attr("hour-data", time)
+    input.attr("data-hour", time)
     plannerDiv.append(input);
 
     //Creating save button for each row
     var save = $("<div>");
     save.addClass("col-lg-1 save");
-    save.attr("hour-data", time)
+    save.attr("data-hour", time)
     //save.attr("input-data", input[time].textContent);
     plannerDiv.append(save);
     save.text("Save");
 
     //Changing color of each row to determine past/present/future 
-    if (timeDiv.attr("hour-data") === moment().format("H")) {
+    if (timeDiv.attr("data-hour") === moment().format("H")) {
         input.css("background-color", "green");
-    } else if (timeDiv.attr("hour-data") > moment().format("H")) {
+    } else if (timeDiv.attr("data-hour") < moment().format("H")) {
         input.css("background-color", "orange");
     }
 
     //Local storage
     save.on("click", function () {
-        var dataHour = $(this).attr("hour-data");
+        var dataHour = $(this).attr("data-hour");
         var content = $(".hour"+time).val()
 
         localStorage.setItem(dataHour, content);
         console.log(dataHour, content);
     })
+    
+    $(".hour"+time).val(localStorage.getItem(time));
 };
 
-//Populate input from local storage
-$(".hour9").append(JSON.parse(localStorage.getItem("9")));
-$(".hour10").append(JSON.parse(localStorage.getItem("10")));
-$(".hour11").append(localStorage.getItem("11"));
-$(".hour12").append(localStorage.getItem("12"));
-$(".hour13").append(localStorage.getItem("13"));
-$(".hour14").append(localStorage.getItem("14"));
-$(".hour15").append(localStorage.getItem("15"));
-$(".hour16").append(localStorage.getItem("16"));
-$(".hour17").append(localStorage.getItem("17"));
+//Populate input from local storage *** Moved inside for loop to prevent repeating myself
+// $(".hour9").val(localStorage.getItem("9"));
+// $(".hour10").val(localStorage.getItem("10"));
+// $(".hour11").val(localStorage.getItem("11"));
+// $(".hour12").val(localStorage.getItem("12"));
+// $(".hour13").val(localStorage.getItem("13"));
+// $(".hour14").val(localStorage.getItem("14"));
+// $(".hour15").val(localStorage.getItem("15"));
+// $(".hour16").val(localStorage.getItem("16"));
+// $(".hour17").val(localStorage.getItem("17"));
